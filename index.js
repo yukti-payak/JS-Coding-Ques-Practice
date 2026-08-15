ff5cf99d8bf45457093b7bb7bd31db0b06980c6d
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-08-15 23:37:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -357,4 +357,19 @@ function longest(sentence){
 }
 
 console.log(longest("I love javascript programming"));
+
+
+
+function missingNumber(arr) {
+  const n = arr.length + 1;
+
+  const expectedSum = (n * (n + 1)) / 2;
+
+  const actualSum = arr.reduce((sum, num) => sum + num, 0);
+
+  return expectedSum - actualSum;
+}
+
+console.log(missingNumber([1, 2, 3, 5, 6]));
+// 4
   
