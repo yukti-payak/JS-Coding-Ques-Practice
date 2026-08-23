a26f921a4e9f3b87f68a560cd5faa7232e5a0d0d
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-08-23 23:36:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -487,4 +487,24 @@ function throttle(func, delay) {
 }
 
 
+function maxSubArray(nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+  
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    
+    
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+// Example usage:
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArray(nums)); 
+
+
   
