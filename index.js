8fa878e81f57a8824aff30b2ae9d2f2e7e3274b8
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-04 22:40:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -630,3 +630,40 @@ const mergedObj = { ...obj1, ...obj2 };
 console.log(mergedObj); 
 
 
+
+function threeSum(nums) {
+  const result = []
+  nums.sort((a, b) => a - b);
+  
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    
+    let left = i + 1;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        
+        // Move both pointers inward
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++; // Need a larger sum
+      } else {
+        right--; // Need a smaller sum
+      }
+    }
+  }
+  
+  return result;
+}
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); 
+
+
