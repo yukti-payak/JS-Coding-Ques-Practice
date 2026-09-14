14dd3053a808f093ef65db885f98d1c3edbe2386
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-14 22:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -723,3 +723,23 @@ console.log(lru.get(3));
 console.log(lru.get(4)); 
 
 
+
+function moveZeroes(nums) {
+  let insertPos = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[insertPos] = nums[i];
+      insertPos++;
+    }
+  }
+  while (insertPos < nums.length) {
+    nums[insertPos] = 0;
+    insertPos++;
+  }
+
+  return nums;
+}
+const arr = [0, 1, 0, 3, 12];
+moveZeroes(arr);
+console.log(arr); 
+
