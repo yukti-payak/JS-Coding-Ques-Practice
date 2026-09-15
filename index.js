71540edb8c0c21fa96279cd2f9f1778a5a6282c6
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-15 22:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -743,3 +743,23 @@ const arr = [0, 1, 0, 3, 12];
 moveZeroes(arr);
 console.log(arr); 
 
+
+function twoSum(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+
+    return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); 
+console.log(twoSum([3, 2, 4], 6));      
+
