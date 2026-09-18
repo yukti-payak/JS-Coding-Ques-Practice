eeb4a4a916fb4b67d1aca125d728b11eb09485f9
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-18 22:56:01 +0530

    feat(algo): implement two-pointer solution for Container With Most Water

--- index.js
+++ index.js
@@ -807,8 +807,29 @@ function sortZerosAndOnes(nums) {
     return nums;
 }
 
-// Example usage:
+
 const colors = [1, 0, 1, 1, 0, 0, 1, 0];
 console.log(sortZerosAndOnes(colors)); 
+
+function maxArea(height) {
+    let maxWater = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        const width = right - left;
+        const minHeight = Math.min(height[left], height[right]);
+
+        const currentWater = width * minHeight;
+        maxWater = Math.max(maxWater, currentWater);
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxWater;
+}
 
 
