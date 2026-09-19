0ee95ccdfd061ee151666a98a3973fcc9b127d45
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-19 21:52:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -833,3 +833,48 @@ function maxArea(height) {
 }
 
 
+    function  boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+            
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1; 
+                } else {
+                    low = mid + 1;  
+                }
+            }
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;  
+                } else {
+                    high = mid - 1; 
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        int[] nums = {2, 5, 6, 0, 0, 1, 2};
+        
+        System.out.println(solver.search(nums, 0)); 
+        System.out.println(solver.search(nums, 3)); 
+    }
+}
+
+
+
