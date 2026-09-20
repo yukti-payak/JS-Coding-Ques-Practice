0ad8c6bc92470a882d47a06aa0ab0ca3b088d11d
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-20 21:12:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -878,3 +878,45 @@ function maxArea(height) {
 
 
 
+function search(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] === target) {
+      return true;
+    }
+
+    if (nums[left] === nums[mid] && nums[mid] === nums[right]) {
+      left++;
+      right--;
+      continue;
+    }
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } 
+ 
+    else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(search([2, 5, 6, 0, 0, 1, 2], 0)); 
+console.log(search([2, 5, 6, 0, 0, 1, 2], 3)); 
+console.log(search([1, 0, 1, 1, 1], 0));       
+
+
+
