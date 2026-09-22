aa56e0ebbd564ef3710fa1ad52124caa7fc20f2a
yukti payak <137981511+yukti-payak@users.noreply.github.com>
2026-09-22 21:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -946,3 +946,23 @@ console.log(binarySearch([1, 3, 5, 7, 9, 11, 15], 10));
 
 
 
+function findPeakElement(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } 
+        
+        else {
+            right = mid;
+        }
+    }
+
+    
+    return left;
+}
+
+console.log(findPeakElement([1, 2, 3, 1])); 
